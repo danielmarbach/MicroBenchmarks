91b13cb0f279533c3dbaaddd643df2e15b8d1ea9
Daniel Marbach <daniel.marbach@particular.net>
2016-08-29 16:31:39 +0200

    HashSet vs List contains

---
+++ MicroBenchmarks/Types/EnumerableVsList.cs
@@ -0,0 +1,49 @@
+using System.Collections.Generic;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Columns;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Diagnostics.Windows;
+using BenchmarkDotNet.Exporters;
+
+namespace MicroBenchmarks.Linq
+{
+    [Config(typeof(Config))]
+    public class HashSetVsListWithContains
+    {
+        private class Config : ManualConfig
+        {
+            public Config()
+            {
+                Add(MarkdownExporter.GitHub);
+                //Add(new MemoryDiagnoser());
+                Add(StatisticColumn.AllStatistics);
+            }
+        }
+        [Params(2, 4, 8, 16, 32, 64)]
+        public int Elements { get; set; }
+
+        [Benchmark(Baseline = true)]
+        public void HashSet()
+        {
+            var hashSet = new HashSet<string>();
+            for (int i = 0; i < Elements; i++)
+            {
+                hashSet.Add(i.ToString());
+            }
+        }
+
+        [Benchmark]
+        public void ListWithContains()
+        {
+            var hashSet = new List<string>();
+            for (int i = 0; i < Elements; i++)
+            {
+                var value = i.ToString();
+                if (!hashSet.Contains(value))
+                {
+                    hashSet.Add(value);
+                }
+            }
+        }
+    }
+}
