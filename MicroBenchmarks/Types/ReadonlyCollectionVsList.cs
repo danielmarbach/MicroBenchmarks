18b31d836a20b1fb3b9bc78c30d56fdf3adf1db4
Daniel Marbach <daniel.marbach@particular.net>
2016-09-07 17:14:00 +0200

    ReadonlyCollectionVsList

---
+++ MicroBenchmarks/Types/ReadonlyCollectionVsList.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Columns;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Diagnostics.Windows;
+using BenchmarkDotNet.Exporters;
+
+namespace MicroBenchmarks.Linq
+{
+    [Config(typeof(Config))]
+    public class ReadonlyCollectionVsList
+    {
+        private List<int> list;
+        private ReadOnlyCollection<int> readonlyList;
+
+        private class Config : ManualConfig
+        {
+            public Config()
+            {
+                Add(MarkdownExporter.GitHub);
+                Add(new MemoryDiagnoser());
+                Add(StatisticColumn.AllStatistics);
+            }
+        }
+        [Params(2, 4, 8, 16, 32, 64)]
+        public int Elements { get; set; }
+
+        [Setup]
+        public void SetUp()
+        {
+            list = Enumerable.Range(0, Elements).ToList();
+            readonlyList = new ReadOnlyCollection<int>(list);
+        }
+
+        [Benchmark(Baseline = true)]
+        public void List()
+        {
+            foreach (var i in list)
+            {
+                GC.KeepAlive(i);
+            }
+        }
+
+        [Benchmark]
+        public void ReadonlyCollection()
+        {
+            foreach (var i in readonlyList)
+            {
+                GC.KeepAlive(i);
+            }
+        }
+    }
+}
