45aece05cc958911894868f544b0c68a22eb2fd4
danielmarbach <daniel.marbach@openplace.net>
2017-12-07 12:06:37 +0100

    Eliding

---
+++ MicroBenchmarks/Async/Eliding.cs
@@ -0,0 +1,37 @@
+﻿using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Columns;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Exporters;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Toolchains.CsProj;
+
+namespace MicroBenchmarks.Async
+{
+    [Config(typeof(Config))]
+    public class Eliding
+    {
+        private class Config : ManualConfig
+        {
+            public Config()
+            {
+                Add(MarkdownExporter.GitHub);
+                Add(new BenchmarkDotNet.Diagnosers.MemoryDiagnoser());
+                Add(StatisticColumn.AllStatistics);
+                Add(Job.Default.With(CsProjCoreToolchain.NetCoreApp20));
+            }
+        }
+
+        [Benchmark(Baseline = true)]
+        public async Task WithStatemachine()
+        {
+            await Task.Delay(1);
+        }
+
+        [Benchmark]
+        public Task WithoutStatemachine()
+        {
+            return Task.Delay(1);
+        }
+    }
+}
