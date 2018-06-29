6f3cf72f13adb12b94ecea10efd5256ae99373d3
danielmarbach <daniel.marbach@openplace.net>
2018-06-29 08:52:33 +0200

    ToString or Equals

---
+++ MicroBenchmarks/LowLevel/ObjectToStringOrEqual.cs
@@ -0,0 +1,39 @@
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Columns;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Environments;
+using BenchmarkDotNet.Exporters;
+using BenchmarkDotNet.Jobs;
+
+namespace MicroBenchmarks.LowLevel
+{
+    [Config(typeof(Config))]
+    public class ObjectToStringOrEqual
+    {
+        private class Config : ManualConfig
+        {
+            public Config()
+            {
+                Add(MarkdownExporter.GitHub);
+                Add(new BenchmarkDotNet.Diagnosers.MemoryDiagnoser());
+                Add(StatisticColumn.AllStatistics);
+                Add(Job.Default.With(Platform.X64));
+                Add(Job.Default.With(Platform.X86));
+            }
+        }
+
+        private object someStringDisguisedAsObject = "somestring";
+
+        [Benchmark(Baseline = true)]
+        public bool ObjectToString()
+        {
+            return someStringDisguisedAsObject.ToString() == "somestring";
+        }
+        
+        [Benchmark()]
+        public bool ObjectEquals()
+        {
+            return someStringDisguisedAsObject.Equals("somestring");
+        }
+    }
+}
