1ee4fc0ba08d7a516a075d4023cdd40ab56545fa
danielmarbach <daniel.marbach@openplace.net>
2018-06-29 09:29:16 +0200

    Playing around with buffers

---
+++ MicroBenchmarks/Buffers/Buffers.cs
@@ -0,0 +1,94 @@
+﻿using System.Buffers;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Columns;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Engines;
+using BenchmarkDotNet.Environments;
+using BenchmarkDotNet.Exporters;
+using BenchmarkDotNet.Jobs;
+
+namespace MicroBenchmarks.Buffers
+{
+    [Config(typeof(Config))]
+    public class Buffers
+    {
+        private class Config : ManualConfig
+        {
+            public Config()
+            {
+                Add(MarkdownExporter.GitHub);
+                Add(new BenchmarkDotNet.Diagnosers.MemoryDiagnoser());
+                Add(StatisticColumn.AllStatistics);
+                Add(Job.Default.With(Platform.X64).With(new GcMode()
+                {
+                    Force = false // tell BenchmarkDotNet not to force GC collections after every iteration
+                }));
+            }
+        }
+        
+        [Params(1,
+            2,
+            8, 
+            16, 
+            32, 
+            64)] 
+        public int Size { get; set; }
+
+        private ArrayPool<Task> taskListPool;
+        private DeadSimpleDictionaryBuffer dictionaryBuffer;
+
+        [GlobalSetup]
+        public void GlobalSetup()
+        {
+            taskListPool = ArrayPool<Task>.Create();
+            dictionaryBuffer = new DeadSimpleDictionaryBuffer();
+        }
+
+        [Benchmark(Baseline = true)]
+        public void Task_Allocate() 
+            => DeadCodeEliminationHelper.KeepAliveWithoutBoxing(new Task[Size]);
+        
+        [Benchmark]
+        public void Task_RentAndReturn_Shared()
+        {
+            var pool = ArrayPool<Task>.Shared;
+            var array = pool.Rent(Size);
+            pool.Return(array, clearArray: true);
+        }
+
+        [Benchmark]
+        public void Task_RentAndReturn_Aware()
+        {
+            var pool = taskListPool;
+            var array = pool.Rent(Size);
+            pool.Return(array, clearArray: true);
+        }
+
+        class DeadSimpleDictionaryBuffer
+        {
+            ConcurrentBag<Dictionary<string, string>> buffer = new ConcurrentBag<Dictionary<string, string>>();
+
+            public DeadSimpleDictionaryBuffer()
+            {
+                for (int i = 0; i < 64; i++)
+                {
+                    buffer.Add(new Dictionary<string, string>());
+                }
+            }
+            
+            public Dictionary<string, string> Rent()
+            {
+                buffer.TryTake(out var dictionary);
+                return dictionary;
+            }
+
+            public void Return(Dictionary<string, string> dictionary)
+            {
+                buffer.Add(dictionary);
+            }
+        }
+    }
+}
---
+++ MicroBenchmarks/Buffers/DictionaryBuffers.cs
@@ -0,0 +1,158 @@
+﻿using System.Collections.Concurrent;
+using System.Collections.Generic;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Columns;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Engines;
+using BenchmarkDotNet.Environments;
+using BenchmarkDotNet.Exporters;
+using BenchmarkDotNet.Jobs;
+
+namespace MicroBenchmarks.Buffers
+{
+    [Config(typeof(Config))]
+    public class DictionaryBuffers
+    {
+        private class Config : ManualConfig
+        {
+            public Config()
+            {
+                Add(MarkdownExporter.GitHub);
+                Add(new BenchmarkDotNet.Diagnosers.MemoryDiagnoser());
+                Add(StatisticColumn.AllStatistics);
+                Add(Job.Default.With(Platform.X64).With(new GcMode()
+                {
+                    Force = false // tell BenchmarkDotNet not to force GC collections after every iteration
+                }));
+            }
+        }
+        
+        [Params(1,
+            2,
+            8, 
+            16, 
+            32, 
+            64)] 
+        public int Size { get; set; }
+
+        private DeadSimpleDictionaryBuffer dictionaryBuffer;
+
+        [GlobalSetup]
+        public void GlobalSetup()
+        {
+            dictionaryBuffer = new DeadSimpleDictionaryBuffer();
+        }
+
+        [Benchmark(Baseline = true)]
+        public void Dictionary_Allocate() 
+            => DeadCodeEliminationHelper.KeepAliveWithoutBoxing(new Dictionary<string, string>[Size]);
+
+        [Benchmark]
+        public void Task_RentAndReturn_Aware()
+        {
+            var pool = dictionaryBuffer;
+            var dictionary = pool.Rent(Size);
+            pool.Return(dictionary, Size, clear: true);
+        }
+
+        class DeadSimpleDictionaryBuffer
+        {
+            ConcurrentBag<Dictionary<string, string>> buffer1 = new ConcurrentBag<Dictionary<string, string>>();
+            ConcurrentBag<Dictionary<string, string>> buffer2 = new ConcurrentBag<Dictionary<string, string>>();
+            ConcurrentBag<Dictionary<string, string>> buffer8 = new ConcurrentBag<Dictionary<string, string>>();
+            ConcurrentBag<Dictionary<string, string>> buffer16 = new ConcurrentBag<Dictionary<string, string>>();
+            ConcurrentBag<Dictionary<string, string>> buffer32 = new ConcurrentBag<Dictionary<string, string>>();
+            ConcurrentBag<Dictionary<string, string>> buffer64 = new ConcurrentBag<Dictionary<string, string>>();
+
+            public DeadSimpleDictionaryBuffer()
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    buffer1.Add(new Dictionary<string, string>(1));
+                }
+                
+                for (int i = 0; i < 10; i++)
+                {
+                    buffer2.Add(new Dictionary<string, string>(2));
+                }
+                
+                for (int i = 0; i < 10; i++)
+                {
+                    buffer8.Add(new Dictionary<string, string>(8));
+                }
+                
+                for (int i = 0; i < 10; i++)
+                {
+                    buffer16.Add(new Dictionary<string, string>(16));
+                }
+                
+                for (int i = 0; i < 10; i++)
+                {
+                    buffer32.Add(new Dictionary<string, string>(32));
+                }
+                
+                for (int i = 0; i < 10; i++)
+                {
+                    buffer64.Add(new Dictionary<string, string>(64));
+                }
+            }
+            
+            public Dictionary<string, string> Rent(int size)
+            {
+                Dictionary<string, string> dictionary = null;
+                switch (size)
+                {
+                    case 1:
+                        buffer1.TryTake(out dictionary);
+                        break;
+                    case 2:
+                        buffer2.TryTake(out dictionary);
+                        break;
+                    case 8:
+                        buffer8.TryTake(out dictionary);
+                        break;
+                    case 16:
+                        buffer16.TryTake(out dictionary);
+                        break;
+                    case 32:
+                        buffer32.TryTake(out dictionary);
+                        break;
+                    case 64:
+                        buffer64.TryTake(out dictionary);
+                        break;                    
+                }
+
+                return dictionary;
+            }
+
+            public void Return(Dictionary<string, string> dictionary, int size, bool clear = false)
+            {               
+                if (clear)
+                {
+                    dictionary.Clear();
+                }
+                switch (size)
+                {
+                    case 1:
+                        buffer1.Add(dictionary);
+                        break;
+                    case 2:
+                        buffer2.Add(dictionary);
+                        break;
+                    case 8:
+                        buffer8.Add(dictionary);
+                        break;
+                    case 16:
+                        buffer16.Add(dictionary);
+                        break;
+                    case 32:
+                        buffer32.Add(dictionary);
+                        break;
+                    case 64:
+                        buffer64.Add(dictionary);
+                        break;                    
+                }
+            }
+        }
+    }
+}
