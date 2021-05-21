a404f63c6afe93bc1c172a4b1c6886ca760da5ab
danielmarbach <daniel.marbach@openplace.net>
2021-05-21 11:49:05 +0200

    IL benchmark

---
+++ MicroBenchmarks/LowLevel/ReflectionVsIL.cs
@@ -0,0 +1,68 @@
+﻿namespace MicroBenchmarks.LowLevel
+{
+    using System;
+    using System.Linq;
+    using System.Reflection;
+    using System.Reflection.Emit;
+    using BenchmarkDotNet.Attributes;
+    using BenchmarkDotNet.Configs;
+    using BenchmarkDotNet.Diagnosers;
+    using BenchmarkDotNet.Exporters;
+
+    [Config(typeof(Config))]
+    public class ReflectionVsIL
+    {
+        private FieldInfo messageTemplateBackingField;
+        private Action<LogEvent,MessageTemplate> templateSetter;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            var fields = typeof(LogEvent).GetFields(BindingFlags.Instance | BindingFlags.NonPublic);
+            messageTemplateBackingField = fields.SingleOrDefault(f => f.Name.Contains("<MessageTemplate>"));
+
+            DynamicMethod templateSetterMethod = new DynamicMethod("templateSetter", typeof(void), new[] { typeof(LogEvent), typeof(MessageTemplate) }, typeof(ReflectionVsIL));
+            var ilGenerator = templateSetterMethod.GetILGenerator();
+            // arg0.<field> = arg1
+            ilGenerator.Emit(OpCodes.Ldarg_0);
+            ilGenerator.Emit(OpCodes.Ldarg_1);
+            ilGenerator.Emit(OpCodes.Stfld, messageTemplateBackingField!);
+            ilGenerator.Emit(OpCodes.Ret);
+            templateSetter = (Action<LogEvent,MessageTemplate>) templateSetterMethod.CreateDelegate(typeof(Action<LogEvent,MessageTemplate>));;
+        }
+
+        private class Config : ManualConfig
+        {
+            public Config()
+            {
+                Add(MarkdownExporter.GitHub);
+                Add(MemoryDiagnoser.Default);
+            }
+        }
+
+        [Benchmark(Baseline = true)]
+        public LogEvent SetValue()
+        {
+            var logEvent = new LogEvent();
+            messageTemplateBackingField.SetValue(logEvent, new MessageTemplate());
+            return logEvent;
+        }
+
+        [Benchmark]
+        public LogEvent Emit()
+        {
+            var logEvent = new LogEvent();
+            templateSetter(logEvent, new MessageTemplate());
+            return logEvent;
+        }
+
+        public class LogEvent
+        {
+            public MessageTemplate MessageTemplate { get; }
+        }
+    }
+
+    public class MessageTemplate
+    {
+    }
+}
---
+++ MicroBenchmarks/Regex/RegexCompiledRegexMatch.cs
@@ -0,0 +1,44 @@
+﻿namespace MicroBenchmarks.RegexBenchmarks
+{
+    using System;
+    using System.Text.RegularExpressions;
+    using BenchmarkDotNet.Attributes;
+    using BenchmarkDotNet.Configs;
+    using BenchmarkDotNet.Diagnosers;
+    using BenchmarkDotNet.Exporters;
+
+    [Config(typeof(Config))]
+    public class RegexCompiledRegexMatch
+    {
+        private readonly string jwtRegex = @"eyJhbGciOi[\w\-\.]+";
+        private Regex regex;
+        private const string MaskValue = "***MASKED***";
+
+        private class Config : ManualConfig
+        {
+            public Config()
+            {
+                Add(MarkdownExporter.GitHub);
+                Add(MemoryDiagnoser.Default);
+            }
+        }
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            regex = new Regex(jwtRegex, RegexOptions.Compiled, TimeSpan.FromSeconds(5));
+        }
+
+        [Benchmark(Baseline = true)]
+        public string RegexMatch()
+        {
+            return Regex.Replace("Token eyJhbGciOiblahblah.b0dy-.sig_nature arrived", jwtRegex, MaskValue);
+        }
+
+        [Benchmark]
+        public string Compile()
+        {
+            return regex.Replace("Token eyJhbGciOiblahblah.b0dy-.sig_nature arrived", MaskValue);
+        }
+    }
+}
