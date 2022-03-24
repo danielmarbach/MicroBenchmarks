676cbe30ae4592e0cc7b800d68cdcf0d2fc293bb
Daniel Marbach <daniel.marbach@openplace.net>
2022-03-24 22:54:58 +0100

    StringContainsVsIndexOf

---
+++ MicroBenchmarks/LowLevel/StringContainsVsIndexOf.cs
@@ -0,0 +1,37 @@
+using System;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Columns;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Diagnosers;
+using BenchmarkDotNet.Exporters;
+
+namespace MicroBenchmarks.LowLevel
+{
+    [Config(typeof(Config))]
+    public class StringContainsVsIndexOf
+    {
+        private string MatchingString = "ThisIsAVeryLongStringContainingTheThingWeLookFor__impl";
+        
+        private class Config : ManualConfig
+        {
+            public Config()
+            {
+                AddExporter(MarkdownExporter.GitHub);
+                AddDiagnoser(MemoryDiagnoser.Default);
+                AddColumn(StatisticColumn.AllStatistics);
+            }
+        }
+        
+        [Benchmark(Baseline = true)]
+        public bool Contains()
+        {
+            return MatchingString.Contains("__impl");
+        }
+        
+        [Benchmark]
+        public bool IndexOf()
+        {
+            return MatchingString.AsSpan().IndexOf("__impl".AsSpan()) != -1;
+        }
+    }
+}
